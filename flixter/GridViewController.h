4d6f6ec5cbd8784e23320e11287b6c9bb260f01d
Max Fierro <maxfierrog@gmail.com>
2022-06-20 00:30:40 -0700

    Added tab controller and CollectionView

--- flixter/GridViewController.h
+++ flixter/GridViewController.h
@@ -11,8 +11,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GridViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UICollectionView *movieGridView;
-@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
-
 @end
 
 NS_ASSUME_NONNULL_END
--- flixter/GridViewController.m
+++ flixter/GridViewController.m
@@ -5,7 +5,9 @@
 //  Created by maxfierro on 6/19/22.
 //
 
+#import "UIImageView+AFNetworking.h"
 #import "GridViewController.h"
+#import "GridMovieCell.h"
 
 @interface GridViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
 @property NSDictionary *movieDict;
@@ -16,11 +18,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Set the activity indicator to hide when it stops.
-    [self.activityIndicator hidesWhenStopped];
-    
-    // Set up networking error alert.
     UIAlertController *alert = [UIAlertController
                       alertControllerWithTitle:@"Network Error"
                                        message:@"No internet connection."
@@ -32,32 +29,20 @@
         [self viewDidLoad];
     }];
     [alert addAction:ok];
-    
-    // Which object we are getting data from and delegating to do things.
     self.movieGridView.dataSource = self;
     self.movieGridView.delegate = self;
-    
-    // URL object, apparently that's a thing here.
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=0e3e4cf020820c13098e4a8ddad6a61b"];
-    
-    // Request object, apparently that's also a thing here.
     NSURLRequest *request = [NSURLRequest requestWithURL:url
                             cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
                             timeoutInterval:10.0];
-    
-    // Session object, apparently... no, this one makes sense I guess.
     NSURLSession *session = [NSURLSession sessionWithConfiguration:
                                 [NSURLSessionConfiguration defaultSessionConfiguration]
                             delegate:nil
                             delegateQueue:[NSOperationQueue mainQueue]];
-
-    // URL Session Data Task object. Imagine abstraction.
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request
                                  completionHandler:^(NSData *data,
                                                     NSURLResponse *response,
                                                     NSError *error) {
-        // Start to spin loading animation.
-        [self.activityIndicator startAnimating];
         if (error != nil) {
             NSLog(@"%@", [error localizedDescription]);
             [self presentViewController:alert animated:YES completion:nil];
@@ -66,41 +51,19 @@
                               JSONObjectWithData:data
                                          options:NSJSONReadingMutableContainers
                                            error:nil];
-
-            // An artifact of how the API disbursed the data. We deduced
-            // what goes here by looking at it 'manually'.
             self.movieArray = self.movieDict[@"results"];
-
-            // Generating tables is faster than getting the data; since
-            // we are making tables from the data, we have to refresh
-            // once we have it.
             [self.movieGridView reloadData];
-
-            // Stop spinning loading animation.
-            [self.activityIndicator stopAnimating];
         }
     }];
-    
-    // I believe this tells the task created above to execute.
     [task resume];
-    
-    // Initialize a UIView > UIRefreshControl for refreshing the data.
     UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
-    
-    // High level framework magic?
     [refreshControl addTarget:self
                        action:@selector(doRefresh:)
              forControlEvents:UIControlEventValueChanged];
-    
-    // INserts the refresh control subview into the UI. Why into tableView?
-    // A: Because it is a subclass of UIView, it can be added as a subview
-    // for another view.
-    // What is atIndex?
     [self.movieGridView insertSubview:refreshControl atIndex:0];
 }
 
 - (void)doRefresh:(UIRefreshControl *)refreshControl {
-    // Set up networking error alert.
     UIAlertController *alert = [UIAlertController
                       alertControllerWithTitle:@"Network Error"
                                        message:@"No internet connection."
@@ -140,9 +103,22 @@
     [task resume];
 }
 
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.movieArray.count;
+}
 
-
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    GridMovieCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"GridMovieCell" forIndexPath: indexPath];
+    NSDictionary *currMovie = self.movieArray[indexPath.row];
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURLString = currMovie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString
+                                     stringByAppendingString:posterURLString];
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+    cell.movieGridCellImage.image = nil;
+    [cell.movieGridCellImage setImageWithURL:posterURL];
+    return cell;
+}
 
 /*
 #pragma mark - Navigation
